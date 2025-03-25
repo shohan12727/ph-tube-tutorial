098456a358e9633868be8049442d7e39e5f57c9b
shohan12727 <aishohan001@gmail.com>
2025-03-25 20:43:37 +0600

    done

--- script/script.js
+++ script/script.js
@@ -22,5 +22,57 @@ function displayCategories (categories){
     categoryContainer.appendChild(categoryDiv);
     }
 }
+function loadVideo (){
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(response => response.json())
+    .then(data => displayVideos(data.videos));
+}
 
+// {/* <section id="video-container"></section> */}
+const displayVideos = (videos)  => {
+    const videoContainer = document.getElementById ('video-container');
+    for (let video of videos ){
+        console.log(video);
+        const videoCard = document.createElement('div')
+        videoCard.innerHTML=`
+  <div class="card bg-base-100 ">
+        <figure class="relative">
+          <img
+          class = "w-full h-[150px] object-cover"
+            src="${video.thumbnail}"
+            alt="Shoes" />
+            <span class="absolute bottom-2 right-2 bg-[#171717] px-1 text-sm rounded text-white">3hrs 56 min ago</span>
+        </figure>
+        <div class=" flex gap-3 px-0 py-5">
+          
+         <div class="profile   ">
+            <div class="avatar w-6">
+                <div class="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
+                  <img src="${video.authors[0].profile_picture}" />
+                </div>
+              </div>
+
+         </div>
+         <div class="intro">
+            <h2 class=" text-sm font-semibold">${video.title} </h2>
+            
+            <p class="text-sm text-gray-400 flex gap-1  ">${video.authors[0].profile_name}
+                <img class="h-5 w-5" src="https://img.icons8.com/fluency/48/verified-badge.png" alt="Verified">
+            </p>
+            
+            <p class="text-sm text-gray-400">${video.others.views} views</p>
+         </div> 
+        </div>
+      </div>
+        `;
+        videoContainer.append(videoCard)
+
+    }
+        
+    };
+
+
+
+
+loadVideo();
 loadCategories();
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,23 @@ module.exports = {
     },
     plugins: [],
   };
+
   
+//   {
+//     "category_id": "1001",
+//     "video_id": "aaah",
+//     "thumbnail": "https://i.ibb.co/hY496Db/coloer-of-the-wind.jpg",
+//     "title": "Colors of the Wind",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/6r4cx4P/ethen-clack.png",
+//             "profile_name": "Ethan Clark",
+//             "verified": true
+//         }
+//     ],
+//     "others": {
+//         "views": "233K",
+//         "posted_date": "16090"
+//     },
+//     "description": "Ethan Clark's 'Colors of the Wind' is a vibrant musical exploration that captivates listeners with its rich, expressive melodies and uplifting rhythm. With 233K views, this song is a celebration of nature's beauty and human connection, offering a soothing and enriching experience for fans of heartfelt, nature-inspired music."
+// }
